1df1d5fa4be4e7f06b928a947114d31c1bf71e4b
potados99 <potados99@gmail.com>
2019-09-15 15:44:55 +0900

    Use getopts && new feature -t for test.

---
+++ template/test.c
@@ -0,0 +1,20 @@
+#include "test.h"
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+TEST(cpmpile_test) { 
+	// Define your tests...
+	ASSERT(1 == 1);
+	return true;
+}
+
+/**
+ * Apply changes here after modifying tests.
+ */
+size_t testc = 1;
+
+test_function testv[] = {
+	compile_test
+};
